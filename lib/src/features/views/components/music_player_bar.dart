ab7747c90d7a53a8101f353ac0cc6bb442233597
unknown <delkanoberutu8@gmail.com>
2025-03-22 21:26:51 +0700

    add slider LinearProgressIndicator in top container

--- lib/src/features/views/components/music_player_bar.dart
+++ lib/src/features/views/components/music_player_bar.dart
@@ -22,97 +22,128 @@ class _MusicPlayerBarState extends State<MusicPlayerBar> {
 
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 15),
-      padding: const EdgeInsets.all(10),
+      // padding: const EdgeInsets.all(10),
       color: Colors.grey[400],
       width: double.infinity,
-      child: StreamBuilder(
-        stream: audioPlayer.audioPlayer.positionStream,
-        builder: (context, positionSnapshot) {
-          final position = positionSnapshot.data ?? Duration.zero;
+      child: Column(
+        children: [
+          StreamBuilder(
+            stream: audioPlayer.audioPlayer.positionStream,
+            builder: (context, positionSnapshot) {
+              final position = positionSnapshot.data ?? Duration.zero;
 
-          return StreamBuilder(
-            stream: audioPlayer.audioPlayer.durationStream,
-            builder: (context, durationSnapshot) {
-              final duration = durationSnapshot.data ?? Duration.zero;
+              return StreamBuilder(
+                stream: audioPlayer.audioPlayer.durationStream,
+                builder: (context, durationSnapshot) {
+                  final duration = durationSnapshot.data ?? Duration.zero;
 
-              if (audioPlayer.isBuffering) {
-                return const CircularProgressIndicator();
-              } else {
-                return Column(
-                  children: [
-                    Text(
-                      audioPlayer.currentTitle,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  if (audioPlayer.isBuffering) {
+                    return const CircularProgressIndicator();
+                  } else {
+                    return Column(
                       children: [
-                        Icon(Icons.skip_previous),
-
-                        if (audioPlayer.isPlaying)
-                          GestureDetector(
-                            onTap: () {
-                              audioPlayer.pauseAudio();
-                            },
-                            child: Icon(Icons.pause),
-                          )
-                        else
-                          GestureDetector(
-                            onTap: () {
-                              audioPlayer.audioPlayer.play();
-                            },
-                            child: Icon(Icons.play_arrow),
+                        Container(
+                          alignment: Alignment.topLeft,
+                          // height: 20,
+                          color: Colors.amber,
+                          child: LinearProgressIndicator(
+                            value:
+                                duration.inSeconds > 0
+                                    ? position.inSeconds / duration.inSeconds
+                                    : 0,
+                            backgroundColor: Colors.grey[300],
+                            valueColor: const AlwaysStoppedAnimation<Color>(
+                              Colors.red,
+                            ),
+                            minHeight: 3,
                           ),
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              audioPlayer.currentTitle,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Icon(Icons.skip_previous),
 
-                        Icon(Icons.skip_next),
+                                if (audioPlayer.isPlaying)
+                                  GestureDetector(
+                                    onTap: () {
+                                      audioPlayer.pauseAudio();
+                                    },
+                                    child: Icon(Icons.pause),
+                                  )
+                                else
+                                  GestureDetector(
+                                    onTap: () {
+                                      audioPlayer.audioPlayer.play();
+                                    },
+                                    child: Icon(Icons.play_arrow),
+                                  ),
+
+                                Icon(Icons.skip_next),
+                              ],
+                            ),
+                            SliderTheme(
+                              data: SliderTheme.of(context).copyWith(
+                                thumbShape: const RoundSliderThumbShape(
+                                  enabledThumbRadius: 5,
+                                ),
+                                overlayShape: const RoundSliderOverlayShape(
+                                  overlayRadius: 14,
+                                ),
+                                thumbColor: Colors.red,
+                                activeTrackColor: Colors.red,
+                                inactiveTrackColor: Colors.grey[300],
+                              ),
+                              child: Slider(
+                                min: 0,
+                                max:
+                                    duration.inSeconds.toDouble() > 0
+                                        ? duration.inSeconds.toDouble()
+                                        : 0.1,
+                                value: position.inSeconds.toDouble().clamp(
+                                  0,
+                                  duration.inSeconds.toDouble() > 0
+                                      ? duration.inSeconds.toDouble()
+                                      : 0.1,
+                                ),
+                                onChanged: (value) async {
+                                  final newPosition = Duration(
+                                    seconds: value.toInt(),
+                                  );
+                                  await audioPlayer.audioPlayer.seek(
+                                    newPosition,
+                                  );
+                                },
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 10,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(_formatDuration(position)),
+                                  Text(_formatDuration(duration)),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ],
-                    ),
-                    SliderTheme(
-                      data: SliderTheme.of(context).copyWith(
-                        thumbShape: const RoundSliderThumbShape(
-                          enabledThumbRadius: 5,
-                        ),
-                        overlayShape: const RoundSliderOverlayShape(
-                          overlayRadius: 14,
-                        ),
-                        thumbColor: Colors.red,
-                        activeTrackColor: Colors.red,
-                        inactiveTrackColor: Colors.grey[300],
-                      ),
-                      child: Slider(
-                        min: 0,
-                        max:
-                            duration.inSeconds.toDouble() > 0
-                                ? duration.inSeconds.toDouble()
-                                : 0.1,
-                        value: position.inSeconds.toDouble().clamp(
-                          0,
-                          duration.inSeconds.toDouble() > 0
-                              ? duration.inSeconds.toDouble()
-                              : 0.1,
-                        ),
-                        onChanged: (value) async {
-                          final newPosition = Duration(seconds: value.toInt());
-                          await audioPlayer.audioPlayer.seek(newPosition);
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(_formatDuration(position)),
-                          Text(_formatDuration(duration)),
-                        ],
-                      ),
-                    ),
-                  ],
-                );
-              }
+                    );
+                  }
+                },
+              );
             },
-          );
-        },
+          ),
+        ],
       ),
     );
   }

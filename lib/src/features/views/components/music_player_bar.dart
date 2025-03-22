7165b325fe8f0e71e5fdea699a0fbd21b50b606e
unknown <delkanoberutu8@gmail.com>
2025-03-23 05:06:11 +0700

    add: music bottom sheet

--- lib/src/features/views/components/music_player_bar.dart
+++ lib/src/features/views/components/music_player_bar.dart
@@ -1,4 +1,5 @@
 import 'package:deep_m/src/features/viewmodels/music_provider.dart';
+import 'package:deep_m/src/features/views/utils/music_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -20,128 +21,139 @@ class _MusicPlayerBarState extends State<MusicPlayerBar> {
   Widget build(BuildContext context) {
     final audioPlayer = Provider.of<MusicProvider>(context);
 
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 15),
-      // padding: const EdgeInsets.all(10),
-      color: Colors.grey[400],
-      width: double.infinity,
-      child: Column(
-        children: [
-          StreamBuilder(
-            stream: audioPlayer.audioPlayer.positionStream,
-            builder: (context, positionSnapshot) {
-              final position = positionSnapshot.data ?? Duration.zero;
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          musicBottomSheet(context);
+        });
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.grey[400],
+        ),
+        clipBehavior: Clip.antiAlias,
+        width: double.infinity,
+        child: Column(
+          children: [
+            StreamBuilder(
+              stream: audioPlayer.audioPlayer.positionStream,
+              builder: (context, positionSnapshot) {
+                final position = positionSnapshot.data ?? Duration.zero;
 
-              return StreamBuilder(
-                stream: audioPlayer.audioPlayer.durationStream,
-                builder: (context, durationSnapshot) {
-                  final duration = durationSnapshot.data ?? Duration.zero;
+                return StreamBuilder(
+                  stream: audioPlayer.audioPlayer.durationStream,
+                  builder: (context, durationSnapshot) {
+                    final duration = durationSnapshot.data ?? Duration.zero;
 
-                  if (audioPlayer.isBuffering) {
-                    return const CircularProgressIndicator();
-                  } else {
-                    return Column(
-                      children: [
-                        Container(
-                          alignment: Alignment.topLeft,
-                          child: LinearProgressIndicator(
-                            value:
-                                duration.inSeconds > 0
-                                    ? position.inSeconds / duration.inSeconds
-                                    : 0,
-                            backgroundColor: Colors.grey[300],
-                            valueColor: const AlwaysStoppedAnimation<Color>(
-                              Colors.red,
-                            ),
-                            minHeight: 3,
-                          ),
-                        ),
-                        Column(
-                          children: [
-                            Text(
-                              audioPlayer.currentTitle,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Icon(Icons.skip_previous),
-
-                                if (audioPlayer.isPlaying)
-                                  GestureDetector(
-                                    onTap: () {
-                                      audioPlayer.pauseAudio();
-                                    },
-                                    child: Icon(Icons.pause),
-                                  )
-                                else
-                                  GestureDetector(
-                                    onTap: () {
-                                      audioPlayer.audioPlayer.play();
-                                    },
-                                    child: Icon(Icons.play_arrow),
-                                  ),
-
-                                Icon(Icons.skip_next),
-                              ],
-                            ),
-                            SliderTheme(
-                              data: SliderTheme.of(context).copyWith(
-                                thumbShape: const RoundSliderThumbShape(
-                                  enabledThumbRadius: 5,
-                                ),
-                                overlayShape: const RoundSliderOverlayShape(
-                                  overlayRadius: 14,
-                                ),
-                                thumbColor: Colors.red,
-                                activeTrackColor: Colors.red,
-                                inactiveTrackColor: Colors.grey[300],
+                    if (audioPlayer.isBuffering) {
+                      return const CircularProgressIndicator();
+                    } else {
+                      return Column(
+                        children: [
+                          Container(
+                            alignment: Alignment.topLeft,
+                            child: LinearProgressIndicator(
+                              value:
+                                  duration.inSeconds > 0
+                                      ? position.inSeconds / duration.inSeconds
+                                      : 0,
+                              backgroundColor: Colors.grey[300],
+                              valueColor: const AlwaysStoppedAnimation<Color>(
+                                Colors.red,
                               ),
-                              child: Slider(
-                                min: 0,
-                                max:
+                              minHeight: 2,
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              // Text(
+                              //   audioPlayer.currentTitle,
+                              //   style: TextStyle(fontWeight: FontWeight.bold),
+                              // ),
+                              // Row(
+                              //   mainAxisAlignment:
+                              //       MainAxisAlignment.spaceEvenly,
+                              //   children: [
+                              //     Icon(Icons.skip_previous),
+
+                              //     if (audioPlayer.isPlaying)
+                              //       GestureDetector(
+                              //         onTap: () {
+                              //           audioPlayer.pauseAudio();
+                              //         },
+                              //         child: Icon(Icons.pause),
+                              //       )
+                              //     else
+                              //       GestureDetector(
+                              //         onTap: () {
+                              //           audioPlayer.audioPlayer.play();
+                              //         },
+                              //         child: Icon(Icons.play_arrow),
+                              //       ),
+
+                              //     Icon(Icons.skip_next),
+                              //   ],
+                              // ),
+                              SliderTheme(
+                                data: SliderTheme.of(context).copyWith(
+                                  thumbShape: const RoundSliderThumbShape(
+                                    enabledThumbRadius: 5,
+                                  ),
+                                  overlayShape: const RoundSliderOverlayShape(
+                                    overlayRadius: 14,
+                                  ),
+                                  thumbColor: Colors.red,
+                                  activeTrackColor: Colors.red,
+                                  inactiveTrackColor: Colors.grey[300],
+                                ),
+                                child: Slider(
+                                  min: 0,
+                                  max:
+                                      duration.inSeconds.toDouble() > 0
+                                          ? duration.inSeconds.toDouble()
+                                          : 0.1,
+                                  value: position.inSeconds.toDouble().clamp(
+                                    0,
                                     duration.inSeconds.toDouble() > 0
                                         ? duration.inSeconds.toDouble()
                                         : 0.1,
-                                value: position.inSeconds.toDouble().clamp(
-                                  0,
-                                  duration.inSeconds.toDouble() > 0
-                                      ? duration.inSeconds.toDouble()
-                                      : 0.1,
+                                  ),
+                                  onChanged: (value) async {
+                                    final newPosition = Duration(
+                                      seconds: value.toInt(),
+                                    );
+                                    await audioPlayer.audioPlayer.seek(
+                                      newPosition,
+                                    );
+                                  },
                                 ),
-                                onChanged: (value) async {
-                                  final newPosition = Duration(
-                                    seconds: value.toInt(),
-                                  );
-                                  await audioPlayer.audioPlayer.seek(
-                                    newPosition,
-                                  );
-                                },
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 10,
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 10,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(_formatDuration(position)),
+                                    Text(_formatDuration(duration)),
+                                  ],
+                                ),
                               ),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(_formatDuration(position)),
-                                  Text(_formatDuration(duration)),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    );
-                  }
-                },
-              );
-            },
-          ),
-        ],
+                            ],
+                          ),
+                        ],
+                      );
+                    }
+                  },
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/src/features/views/utils/music_bottom_sheet.dart
@@ -0,0 +1,23 @@
+import 'package:deep_m/src/features/viewmodels/music_provider.dart';
+import 'package:deep_m/src/features/views/components/music_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void musicBottomSheet(BuildContext context) {
+  final audioPlayer = Provider.of<MusicProvider>(context, listen: false);
+
+  showModalBottomSheet(
+    context: context,
+    builder:
+        (context) => Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              audioPlayer.currentTitle,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            MusicSlider(),
+          ],
+        ),
+  );
+}

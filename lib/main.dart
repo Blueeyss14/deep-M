3a2ceca15190bc7aeac20feba779a71909c3789c
unknown <delkanoberutu8@gmail.com>
2025-03-23 11:42:34 +0700

    setup provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,12 @@
-import 'package:deep_m/src/features/viewmodels/bottombar_viemodel.dart';
-import 'package:deep_m/src/features/viewmodels/buffering_audio_provider.dart';
-import 'package:deep_m/src/features/viewmodels/music_provider.dart';
-import 'package:deep_m/src/features/viewmodels/search_song_provider.dart';
 import 'package:deep_m/src/features/views/home/home_page.dart';
+import 'package:deep_m/src/provider/setup_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => BottombarViemodel()),
-        ChangeNotifierProvider(create: (_) => SearchSongProvider()),
-        ChangeNotifierProvider(create: (context) => MusicProvider(context)),
-        ChangeNotifierProvider(create: (context) => BufferingAudio(context)),
-      ],
-      child: const MyApp(),
-    ),
-  );
+  runApp(MyAppProvider.setup(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/src/provider/setup_provider.dart
@@ -0,0 +1,20 @@
+import 'package:deep_m/src/features/viewmodels/bottombar_viemodel.dart';
+import 'package:deep_m/src/features/viewmodels/buffering_audio_provider.dart';
+import 'package:deep_m/src/features/viewmodels/music_provider.dart';
+import 'package:deep_m/src/features/viewmodels/search_song_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MyAppProvider {
+  static MultiProvider setup({required Widget child}) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => BottombarViemodel()),
+        ChangeNotifierProvider(create: (_) => SearchSongProvider()),
+        ChangeNotifierProvider(create: (context) => MusicProvider(context)),
+        ChangeNotifierProvider(create: (context) => BufferingAudio(context)),
+      ],
+      child: child,
+    );
+  }
+}

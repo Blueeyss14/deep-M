b15263df0e90dcc3d900ac0482765a7d29823625
unknown <delkanoberutu8@gmail.com>
2025-03-24 20:23:55 +0700

    update: playlist dialog ui

--- lib/src/features/views/utils/add_playlist_bottom_sheet.dart
+++ lib/src/features/views/utils/add_playlist_bottom_sheet.dart
@@ -6,6 +6,7 @@ void addPlaylistBottomSheet(BuildContext context, Map<String, String> song) {
   final TextEditingController playlistNameController = TextEditingController();
 
   showModalBottomSheet(
+    backgroundColor: Colors.white,
     clipBehavior: Clip.antiAlias,
     isScrollControlled: true,
     shape: const RoundedRectangleBorder(
@@ -23,84 +24,117 @@ void addPlaylistBottomSheet(BuildContext context, Map<String, String> song) {
           children: [
             AppBar(
               elevation: 0,
-              backgroundColor: Colors.transparent,
+              backgroundColor: Colors.amber,
               surfaceTintColor: Colors.transparent,
               title: Text(
                 "Add to Playlist",
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
-            GestureDetector(
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder:
-                      (context) => AlertDialog(
-                        content: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: TextField(
-                                controller: playlistNameController,
-                                decoration: InputDecoration(
-                                  labelText: 'Nama Playlist',
-                                  border: OutlineInputBorder(),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder:
+                              (context) => AlertDialog(
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(16.0),
+                                      child: TextField(
+                                        controller: playlistNameController,
+                                        decoration: InputDecoration(
+                                          labelText: 'Playlist Name',
+                                          border: OutlineInputBorder(),
+                                        ),
+                                      ),
+                                    ),
+                                    Row(
+                                      children: [
+                                        ElevatedButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: Text("Cancel"),
+                                        ),
+                                        ElevatedButton(
+                                          onPressed: () {
+                                            String playlistName =
+                                                playlistNameController.text;
+                                            if (playlistName.isNotEmpty) {
+                                              playlistProvider.addToPlaylist(
+                                                playlistName,
+                                                song,
+                                              );
+                                              Navigator.pop(context);
+                                            }
+                                          },
+                                          child: Text("Create Playlist"),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
                                 ),
                               ),
+                        );
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        color: Colors.transparent,
+                        alignment: Alignment.topLeft,
+                        child: Row(
+                          children: [
+                            Text(
+                              "Create New Playlist",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                            Row(
-                              children: [
-                                ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: Text("Cancel"),
-                                ),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    String playlistName =
-                                        playlistNameController.text;
-                                    if (playlistName.isNotEmpty) {
-                                      playlistProvider.addToPlaylist(
-                                        playlistName,
-                                        song,
-                                      );
-                                      Navigator.pop(context);
-                                    }
-                                  },
-                                  child: Text("Create Playlist"),
-                                ),
-                              ],
-                            ),
+                            const SizedBox(width: 10),
+                            Icon(Icons.add_rounded),
                           ],
                         ),
                       ),
-                );
-              },
-              child: Text("Add this song"),
-            ),
+                    ),
 
-            const SizedBox(height: 16),
+                    const SizedBox(height: 16),
 
-            Expanded(
-              child: ListView(
-                children: List.generate(
-                  playlistProvider.playlists.keys.length,
-                  (index) => GestureDetector(
-                    onTap: () {
-                      String playlistName = playlistProvider.playlists.keys
-                          .elementAt(index);
-                      playlistProvider.addToPlaylist(playlistName, song);
-                      Navigator.pop(context);
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        playlistProvider.playlists.keys.elementAt(index),
+                    Expanded(
+                      child: ListView(
+                        children: List.generate(
+                          playlistProvider.playlists.keys.length,
+                          (index) => GestureDetector(
+                            onTap: () {
+                              String playlistName = playlistProvider
+                                  .playlists
+                                  .keys
+                                  .elementAt(index);
+                              playlistProvider.addToPlaylist(
+                                playlistName,
+                                song,
+                              );
+                              Navigator.pop(context);
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                playlistProvider.playlists.keys.elementAt(
+                                  index,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
               ),
             ),

cda9a0c9595d4887593d7622a0dc2963cc7101ab
unknown <delkanoberutu8@gmail.com>
2025-03-24 18:54:19 +0700

    update lah pokoknya

--- lib/src/provider/setup_provider.dart
+++ lib/src/provider/setup_provider.dart
@@ -1,6 +1,8 @@
 import 'package:deep_m/src/features/viewmodels/bottombar_viemodel.dart';
 import 'package:deep_m/src/features/viewmodels/buffering_audio_provider.dart';
+import 'package:deep_m/src/features/viewmodels/download_song_provider.dart';
 import 'package:deep_m/src/features/viewmodels/music_provider.dart';
+import 'package:deep_m/src/features/viewmodels/playlist_provider.dart';
 import 'package:deep_m/src/features/viewmodels/search_song_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -13,6 +15,8 @@ class MyAppProvider {
         ChangeNotifierProvider(create: (_) => SearchSongProvider()),
         ChangeNotifierProvider(create: (context) => MusicProvider(context)),
         ChangeNotifierProvider(create: (context) => BufferingAudio(context)),
+        ChangeNotifierProvider(create: (context) => DownloadSongProvider()),
+        ChangeNotifierProvider(create: (context) => PlaylistProvider()),
       ],
       child: child,
     );

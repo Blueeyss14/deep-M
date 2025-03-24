d7cc43f1882c6b477109be420a975c0c6be2f553
unknown <delkanoberutu8@gmail.com>
2025-03-24 17:57:45 +0700

    update download in playlist

---
+++ lib/src/features/viewmodels/download_song_provider.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:deep_m/src/features/viewmodels/music_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:provider/provider.dart';
+
+class DownloadSongProvider extends ChangeNotifier {
+  MusicProvider? musicProvider;
+
+  void initMusicProvider(BuildContext context) {
+    musicProvider = Provider.of<MusicProvider>(context, listen: false);
+  }
+
+  Map<String, bool> downloadStatus = {};
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> getAudioFile(String videoId) async {
+    final path = await _localPath;
+    return File('$path/$videoId.mp3');
+  }
+
+  Future<void> downloadAudio(String videoId, String title) async {
+    if (downloadStatus[videoId] == true) return;
+
+    downloadStatus[videoId] = false;
+    notifyListeners();
+
+    try {
+      final manifest = await musicProvider?.youtube.videos.streamsClient
+          .getManifest(videoId);
+      final audioStreams = manifest?.audioOnly.toList();
+      audioStreams?.sort((a, b) => a.bitrate.compareTo(b.bitrate));
+
+      final audioStream = audioStreams!.isNotEmpty ? audioStreams[0] : null;
+
+      if (audioStream == null) {
+        throw Exception('No Audio Stream Found');
+      }
+
+      final file = await getAudioFile(videoId);
+      final fileStream = file.openWrite();
+      final len = audioStream.size.totalBytes;
+      var count = 0;
+
+      await for (final data in musicProvider!.youtube.videos.streamsClient.get(
+        audioStream,
+      ))
+        ;
+    } catch (e) {}
+  }
+}

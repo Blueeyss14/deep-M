17230265f546a4ca4c41ba17fda7a395f296eb12
unknown <delkanoberutu8@gmail.com>
2025-03-25 00:42:42 +0700

    update: ya

--- lib/src/features/views/pages/playlist_page.dart
+++ lib/src/features/views/pages/playlist_page.dart
@@ -14,92 +14,92 @@ class PlaylistPage extends StatelessWidget {
       resizeToAvoidBottomInset: false,
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: playlistProvider.playlists.length,
-          itemBuilder: (context, index) {
-            String playlistName = playlistProvider.playlists.keys.elementAt(
-              index,
-            );
-            List<Map<String, String>> songs =
-                playlistProvider.playlists[playlistName]!;
+        child:
+            playlistProvider.playlists.isNotEmpty
+                ? ListView.builder(
+                  itemCount: playlistProvider.playlists.length,
+                  itemBuilder: (context, index) {
+                    String playlistName = playlistProvider.playlists.keys
+                        .elementAt(index);
+                    List<Map<String, String>> songs =
+                        playlistProvider.playlists[playlistName]!;
 
-            if (playlistName.isNotEmpty) {
-              return ExpansionTile(
-                title: Text(playlistName),
-                children:
-                    songs.map((song) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: GestureDetector(
-                          onTap: () {
-                            final musicProvider = Provider.of<MusicProvider>(
-                              context,
-                              listen: false,
-                            );
-                            musicProvider.playAudio(
-                              context,
-                              song['videoId'] ?? '',
-                              song['title'] ?? 'Tidak Ada',
-                              song['thumbnail'] ?? '',
-                              song['channel'] ?? 'Tidak Ada',
-                              song['description'] ?? 'Tidak Ada',
-                            );
-                          },
-                          child: Row(
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(3),
-                                child: Image.network(
-                                  song['thumbnail'] ?? '',
-                                  width: 48,
-                                  height: 48,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              const SizedBox(width: 12),
-                              Expanded(
+                    return ExpansionTile(
+                      title: Text(playlistName),
+                      children:
+                          songs.map((song) {
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10),
+                              child: GestureDetector(
+                                onTap: () {
+                                  final musicProvider =
+                                      Provider.of<MusicProvider>(
+                                        context,
+                                        listen: false,
+                                      );
+                                  musicProvider.playAudio(
+                                    context,
+                                    song['videoId'] ?? '',
+                                    song['title'] ?? 'Tidak Ada',
+                                    song['thumbnail'] ?? '',
+                                    song['channel'] ?? 'Tidak Ada',
+                                    song['description'] ?? 'Tidak Ada',
+                                  );
+                                },
                                 child: Row(
                                   children: [
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          song['title'] ?? 'Tidak Ada',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                        const SizedBox(height: 2),
-                                        Text(
-                                          song['channel'] ?? 'Tidak Ada',
-                                          style: TextStyle(fontSize: 12),
-                                        ),
-                                      ],
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(3),
+                                      child: Image.network(
+                                        song['thumbnail'] ?? '',
+                                        width: 48,
+                                        height: 48,
+                                        fit: BoxFit.cover,
+                                      ),
                                     ),
-                                    IconButton(
-                                      onPressed: () {
-                                        playlistProvider.removeSongFromPlaylist(
-                                          playlistName,
-                                          song,
-                                        );
-                                      },
-                                      icon: Icon(Icons.delete),
+                                    const SizedBox(width: 12),
+                                    Expanded(
+                                      child: Row(
+                                        children: [
+                                          Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                song['title'] ?? 'Tidak Ada',
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              const SizedBox(height: 2),
+                                              Text(
+                                                song['channel'] ?? 'Tidak Ada',
+                                                style: TextStyle(fontSize: 12),
+                                              ),
+                                            ],
+                                          ),
+                                          IconButton(
+                                            onPressed: () {
+                                              playlistProvider
+                                                  .removeSongFromPlaylist(
+                                                    playlistName,
+                                                    song,
+                                                  );
+                                            },
+                                            icon: Icon(Icons.delete),
+                                          ),
+                                        ],
+                                      ),
                                     ),
                                   ],
                                 ),
                               ),
-                            ],
-                          ),
-                        ),
-                      );
-                    }).toList(),
-              );
-            } else {
-              return Center(child: Text("No Playlist"));
-            }
-          },
-        ),
+                            );
+                          }).toList(),
+                    );
+                  },
+                )
+                : Center(child: Text("No Playlist")),
       ),
     );
   }

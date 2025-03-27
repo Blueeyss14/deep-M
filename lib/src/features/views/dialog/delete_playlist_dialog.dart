344478de2045c26e1bbc66c29a40278820e1040b
unknown <delkanoberutu8@gmail.com>
2025-03-27 22:59:33 +0700

    add: delete playlist dialog

---
+++ lib/src/features/views/dialog/delete_playlist_dialog.dart
@@ -0,0 +1,90 @@
+import 'package:deep_m/src/features/viewmodels/playlist_provider.dart';
+import 'package:deep_m/src/shared/style/custom_color.dart';
+import 'package:flutter/material.dart';
+
+void deletePlaylistDialog(
+  BuildContext context,
+  String playlistName,
+  Map<String, String> song,
+  PlaylistProvider playlistProvider,
+) {
+  showDialog(
+    context: context,
+    builder:
+        (context) => AlertDialog(
+          titlePadding: EdgeInsets.zero,
+          clipBehavior: Clip.antiAlias,
+          backgroundColor: Colors.transparent,
+          actionsPadding: EdgeInsets.zero,
+          insetPadding: EdgeInsets.zero,
+          contentPadding: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(
+            side: BorderSide(color: CustomColor.white3, width: 0.7),
+            borderRadius: BorderRadius.circular(10),
+          ),
+
+          content: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  CustomColor.musicBar1.withAlpha(50),
+                  CustomColor.musicBar2.withAlpha(70),
+                  CustomColor.musicBar3.withAlpha(80),
+                ],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Are you sure delete this song?",
+                    style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                      color: CustomColor.white1,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          "Cancel",
+                          style: TextStyle(color: CustomColor.white1),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: CustomColor.white1,
+                        ),
+                        onPressed: () {
+                          playlistProvider.removeSongFromPlaylist(
+                            playlistName,
+                            song,
+                          );
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          "Delete",
+                          style: TextStyle(color: CustomColor.blackSheet),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+  );
+}
--- lib/src/features/views/pages/playlist_folder_song.dart
+++ lib/src/features/views/pages/playlist_folder_song.dart
@@ -1,9 +1,8 @@
 import 'dart:ui';
 
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:deep_m/src/features/viewmodels/download_song_provider.dart';
-import 'package:deep_m/src/features/viewmodels/music_provider.dart';
 import 'package:deep_m/src/features/viewmodels/playlist_provider.dart';
+import 'package:deep_m/src/features/views/dialog/delete_playlist_dialog.dart';
 import 'package:deep_m/src/shared/style/custom_color.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -15,8 +14,8 @@ class PlaylistFolderSong extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final playlistProvider = Provider.of<PlaylistProvider>(context);
-    final downloadSongProvider = Provider.of<DownloadSongProvider>(context);
-    final musicProvider = Provider.of<MusicProvider>(context, listen: false);
+    // final downloadSongProvider = Provider.of<DownloadSongProvider>(context);
+    // final musicProvider = Provider.of<MusicProvider>(context, listen: false);
     final songs = playlistProvider.playlists[playlistName] ?? [];
     return Stack(
       fit: StackFit.expand,
@@ -55,12 +54,12 @@ class PlaylistFolderSong extends StatelessWidget {
                     itemCount: songs.length,
                     itemBuilder: (context, songIndex) {
                       final song = songs[songIndex];
-                      final videoId = song['videoId'] ?? '';
-                      final downloadStatus =
-                          downloadSongProvider.downloadStatus[videoId];
-                      final isPlayingThisSong =
-                          musicProvider.isPlaying &&
-                          musicProvider.currentTitle == song['title'];
+                      // final videoId = song['videoId'] ?? '';
+                      // final downloadStatus =
+                      //     downloadSongProvider.downloadStatus[videoId];
+                      // final isPlayingThisSong =
+                      //     musicProvider.isPlaying &&
+                      //     musicProvider.currentTitle == song['title'];
 
                       return ClipRect(
                         clipBehavior: Clip.antiAlias,
@@ -111,20 +110,36 @@ class PlaylistFolderSong extends StatelessWidget {
                                         Text(
                                           song['title'] ?? '',
                                           overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                            fontSize: 13,
+                                            fontWeight: FontWeight.bold,
+                                            color: CustomColor.white1,
+                                          ),
                                         ),
                                         Text(
                                           song['channel'] ?? '',
                                           overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            color: CustomColor.white2,
+                                          ),
                                         ),
                                       ],
                                     ),
                                   ),
                                   GestureDetector(
                                     onTap: () {
-                                      playlistProvider.removeSongFromPlaylist(
+                                      deletePlaylistDialog(
+                                        context,
                                         playlistName,
                                         song,
+                                        playlistProvider,
                                       );
+
+                                      // playlistProvider.removeSongFromPlaylist(
+                                      //   playlistName,
+                                      //   song,
+                                      // );
                                     },
                                     child: Padding(
                                       padding: const EdgeInsets.only(left: 5),
@@ -140,111 +155,6 @@ class PlaylistFolderSong extends StatelessWidget {
                           ),
                         ),
                       );
-
-                      // return Card(
-                      //   margin: EdgeInsets.only(bottom: 12),
-                      //   elevation: 2,
-                      //   child: GestureDetector(
-                      //     onTap: () {
-                      //       musicProvider.startPlaylist(
-                      //         context,
-                      //         playlistName,
-                      //         songIndex,
-                      //       );
-                      //     },
-                      //     child: Padding(
-                      //       padding: const EdgeInsets.all(12.0),
-                      //       child: Row(
-                      //         children: [
-                      //           Stack(
-                      //             alignment: Alignment.center,
-                      //             children: [
-                      //               ClipRRect(
-                      //                 borderRadius: BorderRadius.circular(6),
-                      //                 child: Image.network(
-                      //                   song['thumbnail'] ?? '',
-                      //                   width: 60,
-                      //                   height: 60,
-                      //                   fit: BoxFit.cover,
-                      //                   errorBuilder: (
-                      //                     context,
-                      //                     error,
-                      //                     stackTrace,
-                      //                   ) {
-                      //                     return Container(
-                      //                       width: 60,
-                      //                       height: 60,
-                      //                       color: Colors.grey[300],
-                      //                       child: Icon(Icons.music_note),
-                      //                     );
-                      //                   },
-                      //                 ),
-                      //               ),
-                      //               if (isPlayingThisSong)
-                      //                 Container(
-                      //                   width: 60,
-                      //                   height: 60,
-                      //                   color: Colors.black54,
-                      //                   child: Icon(
-                      //                     Icons.play_arrow,
-                      //                     color: Colors.white,
-                      //                     size: 30,
-                      //                   ),
-                      //                 ),
-                      //             ],
-                      //           ),
-                      //           const SizedBox(width: 16),
-                      //           Expanded(
-                      //             child: Column(
-                      //               crossAxisAlignment:
-                      //                   CrossAxisAlignment.start,
-                      //               children: [
-                      //                 Text(
-                      //                   song['title'] ?? 'Tidak Ada',
-                      //                   style: TextStyle(
-                      //                     fontWeight: FontWeight.bold,
-                      //                     color:
-                      //                         isPlayingThisSong
-                      //                             ? Theme.of(
-                      //                               context,
-                      //                             ).primaryColor
-                      //                             : null,
-                      //                   ),
-                      //                   maxLines: 1,
-                      //                   overflow: TextOverflow.ellipsis,
-                      //                 ),
-                      //                 const SizedBox(height: 4),
-                      //                 Text(
-                      //                   song['channel'] ?? 'Tidak Ada',
-                      //                   style: TextStyle(fontSize: 12),
-                      //                   maxLines: 1,
-                      //                   overflow: TextOverflow.ellipsis,
-                      //                 ),
-                      //                 const SizedBox(height: 4),
-                      //                 _buildDownloadStatus(
-                      //                   context,
-                      //                   downloadStatus,
-                      //                   videoId,
-                      //                   song['title'] ?? 'Tidak Ada',
-                      //                 ),
-                      //               ],
-                      //             ),
-                      //           ),
-                      //           IconButton(
-                      //             onPressed: () {
-                      //               playlistProvider.removeSongFromPlaylist(
-                      //                 playlistName,
-                      //                 song,
-                      //               );
-                      //             },
-                      //             icon: Icon(Icons.delete),
-                      //             tooltip: 'Hapus dari playlist',
-                      //           ),
-                      //         ],
-                      //       ),
-                      //     ),
-                      //   ),
-                      // );
                     },
                   ),
         ),
